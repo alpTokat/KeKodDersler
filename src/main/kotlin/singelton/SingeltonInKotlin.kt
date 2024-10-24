d69cfddd99db964fad0a331745f28faab9852adb
AlpTokat <alpili-tokat@hotmail.com>
2024-10-24 21:21:47 +0300

    object in kotlin

--- src/main/kotlin/singelton/SingeltonInKotlin.kt
+++ src/main/kotlin/singelton/SingeltonInKotlin.kt
@@ -16,17 +16,31 @@ object RetrofitKotlin{
  *  bir nesnenin degiskene esitlenmesiyle olusturulur.
  */
 
-interface TextWatcher{
+interface TextWatcherInterface {
     fun beforeTextChanged(s:CharSequence, start:Int,count:Int,after:Int)
     fun afterTextChanged(s:CharSequence)
     fun onTextChanged(s: CharSequence, start:Int,before:Int,count:Int)
+}
+open class TextWatcherClass: TextWatcherInterface{
+    val surName:String = "asdasdasd"
+    override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun afterTextChanged(s: CharSequence) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+        TODO("Not yet implemented")
+    }
 }
 
 fun main() {
     RetrofitKotlin.baseUrl = "http://www.youtube.com"
 
     // object expression
-    val textWatcher = object: TextWatcher { // isimsiz bir interface ya da class olusturuyoruz.
+    val textWatcherInterface = object: TextWatcherInterface { // isimsiz bir interface ya da class olusturuyoruz.
         override fun beforeTextChanged(
             s: CharSequence,
             start: Int,
@@ -45,6 +59,36 @@ fun main() {
         }
 
     }
+
+    class Anonymous : TextWatcherClass(){
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            super.beforeTextChanged(s, start, count, after) // interface'de body olmadığı icin classtakini işaret eder
+            super<TextWatcherClass>.beforeTextChanged(s, start, count, after) // hangi class yada interface'in olacağı
+        }
+    }
+
+    open class A(){
+        val age:Int = 34
+    }
+
+    class Deneme():A(){
+        val name:String = "Alp"
+
+        private fun asdfds(){
+            age
+        }
+
+        fun demo(){
+            val textWatcher = object : TextWatcherClass() {
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                    println("$age, $name, $surName")
+                }
+
+            }
+        }
+    }
+
+
 }
 
 
